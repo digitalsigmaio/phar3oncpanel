db1b156ccbd710f0f3a4efa998d30745a8995c73
Manson Xasthur <mansonx13@gmail.com>
2017-11-01 17:40:20 +0200

    index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,25 +26,25 @@
 		</div>
 	</div>
 	<div class="row">
-			<div class="row">
-				@if(session()->get('message'))
-					<div class="alert alert-dismissible alert-success">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<strong>{{ session()->get('message') }}</strong>.
-					</div>
-				@endif
-			</div>
-			@if(count($errors) > 0)
-				<div class="row">
-					<div class="alert alert-dismissible alert-warning">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<ul>
-							@foreach($errors->all() as $error)
-								<li>{{ $error }}</li>
-							@endforeach
-						</ul>
-					</div>
+		<div class="row">
+			@if(session()->get('message'))
+				<div class="alert alert-dismissible alert-success">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>{{ session()->get('message') }}</strong>.
 				</div>
 			@endif
 		</div>
+		@if(count($errors) > 0)
+			<div class="row">
+				<div class="alert alert-dismissible alert-warning">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<ul>
+						@foreach($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		@endif
+	</div>
 @endsection
